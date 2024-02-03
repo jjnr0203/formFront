63cbef3b45c709608ac668af2b1fe31486e15e54
jjnr0203 <jonathan08192016@gmail.com>
2024-02-02 19:28:33 -0500

    rutas creadas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/pages/home/home.component';
+import { FormularioComponent } from './app/pages/formulario/formulario.component';
+import { DatesComponent } from './app/pages/dates/dates.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
